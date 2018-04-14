2ae9fdab0a6764022420ff564e3fbe37373a039f
shakirandagire <ndagire.shakira@gmail.com>
2018-04-14 20:19:38 +0300

    Add tests for the application

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,12 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 from api.config import app_config
 
 app = Flask(__name__)
+CORS(app)
 
 config_name = os.environ.get('APP_SETTINGS', 'development')
 app.config.from_object(app_config.get(config_name))
---
+++ api/songs/tests.py
@@ -0,0 +1,75 @@
+import unittest
+import os
+import json
+import flask
+
+from api import db,app
+from .models import Songs
+
+class SongTestCase(unittest.TestCase):
+    """This class represents the songs test case"""
+
+    def setUp(self):
+        """Stuff to do before every test."""
+        self.app = config_name="testing"
+        self.client = app.test_client()
+        self.song = {'title': 'Listen',
+                     'artist': 'Beyonce'}
+        # binds the app to the current context
+        with app.app_context():
+        # Create tables in testdb
+            db.create_all()
+            
+
+    def tearDown(self):
+        """teardown all initialized variables."""
+        with app.app_context():
+            # drop all tables
+            db.session.remove()
+            db.drop_all()
+
+    def test_add_song(self):
+        """Test that user can add songs."""
+        result = self.client.post(
+            '/api/songs',
+            data=self.song)
+        self.assertEqual(result.status_code, 201)
+
+    def test_get_songs(self):
+        """Test that user can get all songs."""
+        result = self.client.post(
+            '/api/songs',
+            data=self.song)
+        result = self.client.get(
+            '/api/songs',
+            data=self.song)
+        self.assertEqual(result.status_code, 200)
+
+    def test_single_song(self):
+        """Test that user can get a single song."""
+        result = self.client.post(
+            '/api/songs',
+            data=self.song)
+        result = self.client.get(
+            '/api/songs/1',
+            data=self.song)
+        self.assertEqual(result.status_code, 200)
+
+    def test_delete_song(self):
+        """Test that user can delete all songs."""
+        result = self.client.post(
+            '/api/songs/1',
+            data=self.song)
+        result = self.client.delete(
+            '/api/songs/1')
+        self.assertEqual(result.status_code, 200)
+    
+    def test_edit_song(self):
+        """Test that user can edit a song."""
+        result = self.client.post(
+            '/api/songs',
+            data=self.song)
+        result = self.client.put(
+            '/api/songs/1',
+            data={'title': 'love you', 'artist': 'Shakira'})
+        self.assertEqual(result.status_code, 201)
